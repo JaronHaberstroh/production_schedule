64c6d3893cdb9656415a59f401a5e10471051858
JHSpaz988 <JHSpaz988@gmail.com>
2024-03-19 15:20:37 -0400

    Establish mongoose model relations and add custom error messages for required fields

--- server/src/models/linePosition.js
+++ server/src/models/linePosition.js
@@ -3,7 +3,7 @@ import mongoose, { Schema, model } from "mongoose";
 export const linePositionSchema = new Schema({
   productionLine: {
     type: mongoose.Types.ObjectId,
-    required: [true, "Line relation is required"],
+    required: [true, "Production line relation is required"],
     ref: "ProductionLine",
   },
   workPositions: [
